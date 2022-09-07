1fd678a7deea57d0b3c317a0922c90ff2cad209c
Diego Valles <dgvall134@gmail.com>
2022-09-06 20:14:57 -0600

    commit tree 390150f89d293d4ee104ae9debdeb8df53418065

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+
+
+function superbowlWin(array) {
+    const arrObj = array.find(element => element.result === `W`)
+  if (!!arrObj === true) {
+    return arrObj.year
+} else return undefined
+}
+
+superbowlWin(record)
